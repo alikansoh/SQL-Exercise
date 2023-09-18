209da02135dc61f0f530e2bc283dfb965a28be3c
alikansoh <alikansoh.dev@gmail.com>
2023-09-18 12:12:56 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,24 @@
+1. SELECT name from students;
+
+2.SELECT * from students
+WHERE age >30;
+
+
+3.select name from students
+where Gender='F' and Age=30
+
+4.SELECT  points from Students
+WHERE name ='Alex'
+
+
+5.INSERT INTO students (name,Age,Gender,Points)
+    VALUES('Ali kansoh',24,'M',300)
+
+6.UPDATE students
+set points=500
+WHERE name="Basma"
+
+7.UPDATE students
+set points=points-100
+WHERE name="Alex"
+
